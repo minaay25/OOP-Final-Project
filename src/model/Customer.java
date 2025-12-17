9d748fd383397930e191b9f7d66db19773f36b92
Mina Ay <minaay005@gmail.com>
2025-12-17 14:10:34 +0300

    Add Customer class to represent renters in the system

---
+++ src/model/Customer.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Customer {
+
+    private String name;
+    private String customerId;
+
+    public Customer(String name, String customerId) {
+        this.name = name;
+        this.customerId = customerId;
+    }
+}
