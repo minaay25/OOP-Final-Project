23dca126dec49d7bd80a6f68dac91a6c18e71cc6
Mina Ay <minaay005@gmail.com>
2025-12-17 13:49:58 +0300

    It's not impo I added so explanations

--- src/interfacepkg/Rentable.java
+++ src/interfacepkg/Rentable.java
@@ -2,7 +2,9 @@ package interfacepkg;
 
 public interface Rentable {
 
-    double calculateRentalFee(int days);
+    double calculateRentalFee(int days);//Add rental fee calculation contract to Rentable interface
 
-    boolean isAvailable();
+
+    boolean isAvailable();//Add availability check contract for rentable objects
+
 }
