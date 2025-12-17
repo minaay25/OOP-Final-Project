b7447041e51b9e2fd527d4f3765c0257bd48daa6
Mina Ay <minaay005@gmail.com>
2025-12-17 13:53:57 +0300

    Add availability tracking and rent/return behavior to Car class

--- src/model/Car.java
+++ src/model/Car.java
@@ -5,10 +5,24 @@ public abstract class Car {
     protected String id;
     protected String brand;
     protected double pricePerDay;
+    protected boolean available;
 
     public Car(String id, String brand, double pricePerDay) {
         this.id = id;
         this.brand = brand;
         this.pricePerDay = pricePerDay;
+        this.available = true;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void rent() {
+        available = false;
+    }
+
+    public void returnCar() {
+        available = true;
     }
 }
