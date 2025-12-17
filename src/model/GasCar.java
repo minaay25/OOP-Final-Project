39867fb0cff192dfb03899d7dfc0d843a753be8d
Mina Ay <minaay005@gmail.com>
2025-12-17 14:07:15 +0300

    missing code

--- src/model/GasCar.java
+++ src/model/GasCar.java
@@ -1,5 +1,11 @@
 package model;
 
-public class GasCar {
+public class GasCar extends Car {
 
+    private double fuelConsumption;
+
+    public GasCar(String id, String brand, double pricePerDay, double fuelConsumption) {
+        super(id, brand, pricePerDay);
+        this.fuelConsumption = fuelConsumption;
+    }
 }
