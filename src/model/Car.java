b9a39021b0f2d8e28eb0d36ac6a538db8290e0b5
Mina Ay <minaay005@gmail.com>
2025-12-17 13:52:49 +0300

    missing code

--- src/model/Car.java
+++ src/model/Car.java
@@ -2,4 +2,13 @@ package model;
 
 public abstract class Car {
 
+    protected String id;
+    protected String brand;
+    protected double pricePerDay;
+
+    public Car(String id, String brand, double pricePerDay) {
+        this.id = id;
+        this.brand = brand;
+        this.pricePerDay = pricePerDay;
+    }
 }
