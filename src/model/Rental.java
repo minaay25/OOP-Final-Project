c8f578ca5dde5cbe3b1d8f061c6898ff804c953e
Mina Ay <minaay005@gmail.com>
2026-01-01 16:21:09 +0300

    Add Javadoc comments to Rental class

--- src/model/Rental.java
+++ src/model/Rental.java
@@ -2,16 +2,36 @@ package model;
 
 import java.time.LocalDate;
 
+/**
+ * Rental class represents a car rental transaction.
+ * Manages rental details including dates, customer, and car information.
+ * 
+ * @author Mina
+ * @version 1.0
+ */
 public class Rental {
-    
+	/** Unique rental identifier */
     private String rentalId;
+    /** Customer who is renting the car */
     private Customer customer;
+    /** Car being rented */
     private Car car;
+    /** Start date of the rental */
     private LocalDate startDate;
+    /** End date of the rental */
     private LocalDate endDate;
+    /** Total cost of the rental */
     private double totalCost;
+    /** Rental active status */
     private boolean isActive;
-    
+    /**
+     * Constructor to create a new rental
+     * @param rentalId Unique rental identifier
+     * @param customer Customer renting the car
+     * @param car Car being rented
+     * @param startDate Rental start date
+     * @param days Number of days for rental
+     */
     public Rental(String rentalId, Customer customer, Car car, LocalDate startDate, int days) {
         this.rentalId = rentalId;
         this.customer = customer;
@@ -21,12 +41,17 @@ public class Rental {
         this.totalCost = car.calculateRentalFee(days);
         this.isActive = true;
     }
-    
+    /**
+     * Complete the rental and return the car
+     */
     public void completeRental() {
         this.isActive = false;
         car.returnCar();
     }
-    
+    /**
+     * Get rental duration in days
+     * @return Number of rental days
+     */
     public int getRentalDays() {
         return (int) java.time.temporal.ChronoUnit.DAYS.between(startDate, endDate);
     }
