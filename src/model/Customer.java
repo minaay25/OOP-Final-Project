b49938dcfd714171d4b4c894c77009a4eea8d0ad
Mina Ay <minaay005@gmail.com>
2026-01-01 16:17:07 +0300

    Add Javadoc comments to Customer class

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,10 +1,20 @@
 package model;
-
+/**
+ * Customer class represents a person who rents cars from the system.
+ * Stores customer information with encapsulation.
+ * 
+ * @author Mina
+ * @version 1.0
+ */
 public class Customer {
-
+	  /** Customer's full name */
     private String name;
     private String customerId;
-
+    /**
+     * Constructor to create a new Customer
+     * @param name Customer's full name
+     * @param customerId Unique customer ID
+     */
     public Customer(String name, String customerId) {
         this.name = name;
         this.customerId = customerId;
