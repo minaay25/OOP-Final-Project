8b2574c0c3a6fbecef9c24550918b5844e497e95
Mina Ay <minaay005@gmail.com>
2026-01-01 16:10:11 +0300

    Add Javadoc comments to ElectricCar class

--- src/model/ElectricCar.java
+++ src/model/ElectricCar.java
@@ -1,5 +1,11 @@
 package model;
-
+/**
+ * ElectricCar extends Car class to represent electric vehicles.
+ * Demonstrates inheritance and polymorphism with eco-friendly discount.
+ * 
+ * @author Mina
+ * @version 1.0
+ */
 public class ElectricCar extends Car {
 
     private double batteryCapacity;
@@ -8,7 +14,12 @@ public class ElectricCar extends Car {
         super(id, brand, pricePerDay);
         this.batteryCapacity = batteryCapacity;
     }
-
+    /**
+     * Calculate rental fee with eco-friendly discount (20% off)
+     * Demonstrates polymorphism - overrides parent method
+     * @param days Number of rental days
+     * @return Total rental cost with eco discount
+     */
     @Override
     public double calculateRentalFee(int days) {
         return days * pricePerDay * 0.9;
